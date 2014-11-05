87b6278004fc70b1fb7c04941fb2f654e425ac6e
samarhxc@gmail.com <Ktkscx1x>
2014-11-06 00:59:02 +0100

    Fix editable.

--- Twig/FroalaEditorExtension.php
+++ Twig/FroalaEditorExtension.php
@@ -29,7 +29,7 @@ class FroalaEditorExtension extends \Twig_Extension
     {
         //------------------------- DECLARE ---------------------------//
     
-        return sprintf( "$( function() { $( \"#%s\" ).editable( { inlineMode : %s } ) } );", $p_id, $p_inlineMode );
+        return sprintf( "$( function() { $( \"#%s\" ).editable( { inlineMode : %s } ) } );", $p_id, $p_inlineMode ? "true" : "false" );
     }
     
     //-------------------------------------------------------------//
