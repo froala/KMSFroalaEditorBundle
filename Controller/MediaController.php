38deee20edb8b24249488830d82af9825c149e5d
samarhxc@gmail.com <Ktkscx1x>
2015-02-22 05:56:07 +0100

    Renaming controller.

---
+++ Controller/MediaController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace KMS\FroalaEditorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Images controller.
+ */
+class MediaController extends Controller
+{
+
+    //-------------------------------------------------------------//
+    //--------------------------- METHODS -------------------------//
+    //-------------------------------------------------------------//
+    
+    /**
+     * Upload an image.
+     */
+    public function uploadImageAction()
+    {
+        $request = $this->get( "request" );
+        $mediaManager = $this->get( "kms_froala_editor.media_manager" );
+        $folder = $request->request->get( "folder" );
+        //------------------------- DECLARE ---------------------------//
+        
+//         if( $request->isXmlHttpRequest() == true )
+//         {
+            return $mediaManager->uploadImage( $request->files, $this->get( "kernel" )->getRootDir(), $request->getBasePath(), $folder );
+            
+//         }
+    }
+    
+    //-------------------------------------------------------------//
+    //--------------------------- PRIVATE -------------------------//
+    //-------------------------------------------------------------//
+    
+}
