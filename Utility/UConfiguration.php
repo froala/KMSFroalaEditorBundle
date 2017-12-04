37f23ce1430135c2b0dfc1788d63798e9579529e
Simon Guionniere <simon.guionniere@gmail.com>
2017-12-04 07:31:15 +0100

    Update to Froala 2.7.2

--- Utility/UConfiguration.php
+++ Utility/UConfiguration.php
@@ -154,7 +154,7 @@
 
 		public static $OPTIONS_STRING_CUSTOM = array(
 			"customJS"          => null, //
-			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor_2.7.1", //
+			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor_2.7.2", //
 			"imageUploadFolder" => "/upload", //
 			"imageUploadPath"   => null, //
 			"fileUploadFolder"  => "/upload", //
