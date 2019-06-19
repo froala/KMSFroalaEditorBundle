d58fd0fc8f8ecc1ebcb3b11d8b976ec417f66e39
jmsche <jonathan.scheiber@siep.be>
2019-06-19 09:32:11 +0200

    Added download command so new versions of editor won't require bundle update

--- Utility/UConfiguration.php
+++ Utility/UConfiguration.php
@@ -166,7 +166,7 @@
 
 		public static $OPTIONS_STRING_CUSTOM = array(
 			"customJS"          => null, //
-			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor_3.0.0", //
+			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor", //
 			"imageUploadFolder" => "/upload", //
 			"imageUploadPath"   => null, //
 			"fileUploadFolder"  => "/upload", //
